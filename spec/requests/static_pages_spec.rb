bca7bae62797d2ebcdd3da424d7dd21b37ffc04f
shuvo266 <shuvo@e-solutionpark.com>
2013-01-06 01:25:25 +0600

    Completed the static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+  describe "Home Page" do
+  	it "should have the content 'Sample App'" do
+  		visit '/static_pages/home'
+  		page.should have_selector('h1',text:'Sample App')
+  	end
+  	describe "Help Page" do
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_selector('h1',text:'Help')
+  	end
+  	end
+    describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1',text:'About Us')
+    end
+    end
+  end
+end
