9484e620ec7df0ff40088517e7a4f004d476e0bd
shuvo266 <shuvo@e-solutionpark.com>
2013-01-16 02:55:35 +0600

    Finish updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-
+  before_filter :signed_in_user, only: [:edit,:update]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+ 
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
   	@user = User.find(params[:id])
@@ -20,4 +26,42 @@ class UsersController < ApplicationController
   		render "new"
   	end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Update Successfull"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "User #{@user.id} is demolished!"
+    redirect_to users_path
+  end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in"
+      end
+    end
+
+    def correct_user
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
+    end
 end
